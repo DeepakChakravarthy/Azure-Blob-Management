1b853125de323216e4789fefeca2309279fab474
DeepakChakravarthy-Kovai <Deepak.chakravarthy@kovai.co>
2022-03-25 20:09:07 +0530

    Commit After Creation

---
+++ index.js
@@ -0,0 +1,74 @@
+const { BlobServiceClient } = require('@azure/storage-blob');
+const { v1: uuidv1} = require('uuid');
+require('dotenv').config()
+
+async function main() {
+    console.log('Azure Blob storage v12 - JavaScript quickstart sample');
+    const AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=job360store;AccountKey=74jBM1s0Zp0EmTx0bV5NJZAvZVG41veLQ5LGqSXIKzIJxnZCjT8NmLt1z6BGVPVSUMFJQbBT+9h++AStIs6Ssg==;EndpointSuffix=core.windows.net"
+
+if (!AZURE_STORAGE_CONNECTION_STRING) {
+  throw Error("Azure Storage Connection string not found");
+}
+// Create the BlobServiceClient object which will be used to create a container client
+  
+  // Create a unique name for the container
+  const containerName = "quickstart" + uuidv1();
+  
+  console.log("\nCreating container...");
+  console.log("\t", containerName);
+  
+  // Get a reference to a container
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  // Create the container
+  const createContainerResponse = await containerClient.create();
+  console.log(
+    "Container was created successfully. requestId: ",
+    createContainerResponse.requestId
+  );
+  // Create a unique name for the blob
+const blobName = "quickstart" + uuidv1() + ".txt";
+
+// Get a block blob client
+const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+console.log("\nUploading to Azure storage as blob:\n\t", blobName);
+
+// Upload data to the blob
+const data = "Hello, World!";
+const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+console.log(
+  "Blob was uploaded successfully. requestId: ",
+  uploadBlobResponse.requestId
+);
+
+console.log("\nListing blobs...");
+
+// List the blob(s) in the container.
+for await (const blob of containerClient.listBlobsFlat()) {
+  console.log("\t", blob.name);
+}
+// Get blob content from position 0 to the end
+// In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
+// In browsers, get downloaded data by accessing downloadBlockBlobResponse.blobBody
+const downloadBlockBlobResponse = await blockBlobClient.download(0);
+console.log("\nDownloaded blob content...");
+console.log(
+  "\t",
+  await streamToText(downloadBlockBlobResponse.readableStreamBody)
+);
+    // Quick start code goes here
+
+}
+// Convert stream to text
+async function streamToText(readable) {
+    readable.setEncoding('utf8');
+    let data = '';
+    for await (const chunk of readable) {
+      data += chunk;
+    }
+    return data;
+  }
+main()
+.then(() => console.log('Done'))
+.catch((ex) => console.log(ex.message));
+
